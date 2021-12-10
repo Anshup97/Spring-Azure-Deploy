a0e2ef30b09e7a05f4d3d2d060841ff31479dcf4
Anshup97 <anshumanp97@gmail.com>
2021-12-10 16:40:23 +0530

    First Commit

---
+++ src/test/java/com/anshuman/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anshuman.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
