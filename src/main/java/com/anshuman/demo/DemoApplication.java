770f76c53dde98a1e962b3cfb63ae1a01d3206ee
Anshup97 <anshumanp97@gmail.com>
2021-12-10 16:58:20 +0530

    Second Commit

--- src/main/java/com/anshuman/demo/DemoApplication.java
+++ src/main/java/com/anshuman/demo/DemoApplication.java
@@ -11,7 +11,8 @@ public class DemoApplication {
 	
 	@GetMapping("/message")
 	public String message() {
-		return "Congrats succcessfully deployed Spring application in Azure";
+		return "Congrats succcessfully deployed Spring application in Azure. "
+				+ "Now you can Fuck off";
 	}
 
 	public static void main(String[] args) {
